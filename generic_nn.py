70ebc1d3462801bbe4dc7f83fa94f1dde355f9ad
Gil Rutter <g.o.rutter@warwick.ac.uk>
2016-02-07 09:24:31 +0000

    Autopep8.

--- generic_nn.py
+++ generic_nn.py
@@ -24,6 +24,7 @@ from keras.optimizers import SGD, Adam, Adadelta
 from keras.utils.visualize_util import plot
 from keras.regularizers import l2
 
+
 class NN_Compare:
     ''' Class allows training and testing multiple neural network architectures
         from disparate Python models on the same data and conveniently
@@ -58,45 +59,46 @@ class NN_Compare:
             ##################
             #  Architecture  #
             ##################
-            'hidden_layer_sizes' : (15,),
-            'activation' : 'relu',
+            'hidden_layer_sizes': (15,),
+            'activation': 'relu',
 
             ####################
             #  Regularisation  #
             ####################
-            'alpha' : 0.0000, # L2 penalty. 0.0 = turned off.
-            'dropout' : 0.0, # !! Dropout between hidden and output layers.
+            'alpha': 0.0000,  # L2 penalty. 0.0 = turned off.
+            'dropout': 0.0,  # !! Dropout between hidden and output layers.
 
             ##############
             #  Learning  #
             ##############
-            'learning_rate_init' : 0.001,
-            'algorithm' : 'sgd',
-            'batch_size' : 16,
+            'learning_rate_init': 0.001,
+            'algorithm': 'sgd',
+            'batch_size': 16,
 
             # SGD only
-            'momentum' : 0.9,
-            'nesterovs_momentum' : False,
+            'momentum': 0.9,
+            'nesterovs_momentum': False,
 
             # Adam only (Scikit-learn and Keras only)
-            'beta_1' : 0.9,
-            'beta_2' : 0.999,
-            'epsilon' : 1e-8,
+            'beta_1': 0.9,
+            'beta_2': 0.999,
+            'epsilon': 1e-8,
 
             #######################
             #  Consistent output  # (for developing and debugging)
             #######################
             # Doesn't work in Keras (there's a hack though -- see the imports)
-            'random_state' : 1,
+            'random_state': 1,
 
             ###############################################
             #  Iterations/epoch settings - DO NOT CHANGE  #
             ###############################################
             # Training iteration is handled manually by this class through the
             # self.iter_settings dict.
-            'warm_start' : True,
-            'max_iter' : 1, # In scikit-learn (and sknn), "iter" really means epoch.
-            'learning_rate' : 'constant',
+            'warm_start': True,
+            # In scikit-learn (and sknn), "iter" really means epoch.
+            'max_iter': 1,
+            'learning_rate': 'constant',
         }
 
         # Iteration settings are homogenised through this dict.
@@ -106,40 +108,41 @@ class NN_Compare:
         self.iter_settings = {
             # Epochs to run for if no convergence. Note that max iterations
             # are not specified but inferred from max_epoch and batch_size
-            'max_epoch' : 3,
+            'max_epoch': 3,
 
             # Max decline in loss between epochs to consider converged. (Ratio)
-            'epoch_tol' : 0.02,
+            'epoch_tol': 0.02,
 
-            # Number of consecutive epochs considered converged before stopping.
-            'n_stable' : 3,
+            # Number of consecutive epochs considered converged before
+            # stopping.
+            'n_stable': 3,
 
             # For SGD, decay in learning rate between epochs. 0 = no decay.
-            #TODO NOTIMPLEMENTEDYET
+            # TODO NOTIMPLEMENTEDYET
             # only implemented in sklearn
-            'learning_decay' : 0.000,
+            'learning_decay': 0.000,
         }
 
         # For settings which take a categorical value, provided is a dict of
         # which settings should work in which of the Python modules.
         # This dict exists only for reference. It is not used for computaton.
         self.supported_settings = {
-            'activation' : {
-                'relu' : ['sklearn', 'sknn', 'keras'],
-                'linear' : ['sknn', 'keras'],
-                'logistic' : ['sklearn', 'sknn', 'keras'],
-                'tanh' : ['sklearn', 'sknn', 'keras']
+            'activation': {
+                'relu': ['sklearn', 'sknn', 'keras'],
+                'linear': ['sknn', 'keras'],
+                'logistic': ['sklearn', 'sknn', 'keras'],
+                'tanh': ['sklearn', 'sknn', 'keras']
             },
 
-            'nesterovs_momentum' : {
-                True : ['sklearn', 'sknn', 'keras'],
-                False : ['sklearn', 'sknn', 'keras']
+            'nesterovs_momentum': {
+                True: ['sklearn', 'sknn', 'keras'],
+                False: ['sklearn', 'sknn', 'keras']
             },
 
-            'algorithm' : {
-                'sgd' : ['sklearn', 'sknn', 'keras'],
-                'adam' : ['sklearn', 'keras'],
-                'adadelta' : ['sknn', 'keras']
+            'algorithm': {
+                'sgd': ['sklearn', 'sknn', 'keras'],
+                'adam': ['sklearn', 'keras'],
+                'adadelta': ['sknn', 'keras']
             },
         }
 
@@ -155,7 +158,8 @@ class NN_Compare:
         '''
 
         if self.nn_settings['algorithm'] != 'sgd' and self.iter_settings['learning_decay'] != 0.0:
-            raise ValueError("The learning_decay option is for the sgd algorithm only.")
+            raise ValueError(
+                "The learning_decay option is for the sgd algorithm only.")
 
     def get_nn_settings(self):
         return self.nn_settings
@@ -179,16 +183,15 @@ class NN_Compare:
 
     def run_test(self, module):
         modules = {
-            'sklearn' : self.sklearn,
-            'sknn'    : self.sknn,
-            'keras'   : self.keras
+            'sklearn': self.sklearn,
+            'sknn': self.sknn,
+            'keras': self.keras
         }
 
         test = {'module': module,
                 'nn_settings': dict(self.nn_settings),
                 'iter_settings': dict(self.iter_settings)
-        }
-
+                }
 
         F_score = 0.0
         percent_score = 0.0
@@ -202,7 +205,8 @@ class NN_Compare:
             X_train = normaliser.transform(X_train)
             X_test = normaliser.transform(X_test)
 
-            predict_proba, loss_curve, valid_curve, model = modules[module](X_train, Y_train, X_test)
+            predict_proba, loss_curve, valid_curve, model = modules[
+                module](X_train, Y_train, X_test)
             test['predict_proba'] = predict_proba
             test['loss_curve'] = loss_curve
             test['valid_curve'] = valid_curve
@@ -225,12 +229,13 @@ class NN_Compare:
         #  Settings conversion  #
         #########################
 
-        activation_dict = {'relu': 'relu', 'linear': 'linear', 'logistic': 'sigmoid', 'tanh': 'tanh'}
+        activation_dict = {'relu': 'relu', 'linear': 'linear',
+                           'logistic': 'sigmoid', 'tanh': 'tanh'}
         try:
             activation = activation_dict[self.nn_settings['activation']]
         except KeyError:
             err_str = "Activation function \"" + self.nn_settings['activation']
-            err_str +=  "\" not supported in Keras."
+            err_str += "\" not supported in Keras."
             raise NotImplementedError(err_str)
 
         if self.nn_settings['dropout'] != 0.0:
@@ -278,7 +283,8 @@ class NN_Compare:
             err_str += "\" not implemented in Keras at present."
             raise NotImplementedError(err_str)
 
-        keras_nn.compile(loss='categorical_crossentropy', class_mode='binary', optimizer=optimiser)
+        keras_nn.compile(loss='categorical_crossentropy',
+                         class_mode='binary', optimizer=optimiser)
 
         ##############
         #  Train NN  #
@@ -321,7 +327,8 @@ class NN_Compare:
             err_str = "The following unsupported settings are not set to 0.0:\n"
             for i, key in enumerate(unsupported_keys):
                 if bad_settings[i]:
-                    err_str += "\t" + key + ": " + str(self.nn_settings[key]) + "\n"
+                    err_str += "\t" + key + ": " + \
+                        str(self.nn_settings[key]) + "\n"
             raise NotImplementedError(err_str)
 
         valid_keys = [
@@ -332,7 +339,8 @@ class NN_Compare:
             'validation_fraction', 'beta_1', 'beta_2', 'epsilon', 'algorithm'
         ]
 
-        sklearn_settings = {key: val for key, val in self.nn_settings.items() if key in valid_keys}
+        sklearn_settings = {key: val for key, val in self.nn_settings.items()
+                            if key in valid_keys}
 
         ###############
         #  Create NN  #
@@ -355,10 +363,10 @@ class NN_Compare:
 
         for i in range(self.iter_settings['max_epoch']):
             sklearn_nn.fit(X_train, Y_train)
-            loss_curve = sklearn_nn.loss_curve_ # sklearn itself keeps a list across fits
+            loss_curve = sklearn_nn.loss_curve_  # sklearn itself keeps a list across fits
 
             learning_rate *= (1.0 - self.iter_settings['learning_decay'])
-            sklearn_nn.set_params(learning_rate_init = learning_rate)
+            sklearn_nn.set_params(learning_rate_init=learning_rate)
 
             # Use change in loss_curve to evaluate stability
             if self.converged(loss_curve, n_loss):
@@ -377,7 +385,8 @@ class NN_Compare:
         #  Settings conversion  #
         #########################
 
-        activation_dict = {'relu': 'Rectifier', 'linear': 'Linear', 'logistic': 'Sigmoid', 'tanh': 'Tanh'}
+        activation_dict = {
+            'relu': 'Rectifier', 'linear': 'Linear', 'logistic': 'Sigmoid', 'tanh': 'Tanh'}
         try:
             activation = activation_dict[self.nn_settings['activation']]
         except KeyError:
@@ -387,13 +396,14 @@ class NN_Compare:
 
         if self.nn_settings['algorithm'] == 'sgd':
             if self.nn_settings['momentum'] == 0.0:
-                learning_rule='sgd'
+                learning_rule = 'sgd'
             elif self.nn_settings['nesterovs_momentum'] == True:
-                learning_rule='nesterov'
+                learning_rule = 'nesterov'
             else:
-                learning_rule='momentum'
+                learning_rule = 'momentum'
         else:
-            raise NotImplementedError("Only SGD is implemented in Scikit-NN at present.")
+            raise NotImplementedError(
+                "Only SGD is implemented in Scikit-NN at present.")
 
         if self.nn_settings['alpha'] != 0.0:
             print "WARNING: I am not convinced that L2 is working in SKNN"
@@ -403,8 +413,9 @@ class NN_Compare:
         # SKNN doesn't give access to the loss in the end-of-epoch callback,
         # only in the end-of-batch callback.
         batch_loss = [0]
+
         def batch_callback(**variables):
-            batch_loss[0] = variables['loss']/variables['count']
+            batch_loss[0] = variables['loss'] / variables['count']
 
         ###############
         #  Create NN  #
@@ -414,7 +425,7 @@ class NN_Compare:
 
             # sknn_nn architecture
             layers=[Layer(activation, units=self.nn_settings['hidden_layer_sizes'][0],),
-                    Layer("Softmax", units=2*self.n_outcomes)],
+                    Layer("Softmax", units=2 * self.n_outcomes)],
 
             # Learning settings
             loss_type='mcc',
@@ -431,7 +442,7 @@ class NN_Compare:
             random_state=self.nn_settings['random_state'],
 
             # Callback to get loss
-            callback = {'on_batch_finish': batch_callback},
+            callback={'on_batch_finish': batch_callback},
 
             # verbose=1
         )
@@ -477,10 +488,11 @@ class NN_Compare:
         false_positives = (predicted_positives & np.logical_not(answers))
         correct_predictions = (predictions == answers)
 
-        precision = float(true_positives.sum())/predicted_positives.sum()
-        recall = float(true_positives.sum())/answers.sum()
-        F1 = (2*precision*recall)/(precision+recall)
-        score = float(correct_predictions.sum())/(predictions.shape[0]*predictions.shape[1])
+        precision = float(true_positives.sum()) / predicted_positives.sum()
+        recall = float(true_positives.sum()) / answers.sum()
+        F1 = (2 * precision * recall) / (precision + recall)
+        score = float(correct_predictions.sum()) / \
+            (predictions.shape[0] * predictions.shape[1])
 
         return F1, score
 
@@ -499,7 +511,7 @@ class NN_Compare:
         '''
 
         try:
-            loss_ratio = 1 - (loss[-1]/loss[-2])
+            loss_ratio = 1 - (loss[-1] / loss[-2])
         except IndexError:
             return False
 
@@ -512,4 +524,3 @@ class NN_Compare:
             return True
         else:
             return False
-
