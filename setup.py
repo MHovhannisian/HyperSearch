bc1215887e0891b8f8944473ad63a43f683eb90e
Gil Rutter <g.o.rutter@warwick.ac.uk>
2016-03-12 15:28:58 +0000

    Setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='HyperSearch',
+    author='Gil Rutter',
+    author_email='gil.rutter@sclgroup.cc',
+    py_modules=['hypersearch', 'unifiedmlp']
+)
